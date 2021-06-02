9451b4b6e7bac2a0e6acaa5d225a43531daa0f2a
totemarcal <totemarcal@gmail.com>
2021-06-02 15:12:06 -0300

    testeDuan

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Aula de GIT.
+          Alôôôôô Duan!
         </p>
         <a
           className="App-link"
